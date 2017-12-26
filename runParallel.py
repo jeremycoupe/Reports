9b412c7dbf5f41bebd24c6b42e7731932cf2a7b3
J Coupe <jeremy.coupe@gmail.com>
2017-12-26 12:01:55 -0800

    updated parallel

--- runParallel.py
+++ runParallel.py
@@ -4,12 +4,10 @@ from fTactical import *
 
 files = os.listdir('opsSummaryDirectoy/originalSummary')
 
-#print(files)
-
-
 threads = []
 
 for item in files:
-	t = threading.Thread(target=fTactical, args=(item,))
-	threads.append(t)
-	t.start()
+	if 'KCLT' in files[item]:
+		t = threading.Thread(target=fTactical, args=(item,))
+		#threads.append(t)
+		t.start()
